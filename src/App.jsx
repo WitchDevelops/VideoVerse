7acfdc34e64da458e20be777e90ca9a5ab4c3451
WitchDevelops <d.n.wojewska@gmail.com>
2023-10-31 18:22:43 +0100

    style navbar and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000", color: "#fff" }} >
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App
